78296f87231481351616883ac55bcf02ede52474
Joao André Simões <jarpsimoes@gmail.com>
2023-02-06 23:38:23 +0000

    ADD: Terraform scripts

---
+++ terraform-iac/main.tf
@@ -0,0 +1,36 @@
+data "azurerm_subnet" "subnet" {
+    name = "operator-net-shr-linux-subnet"
+    virtual_network_name = "operator-net-shr-vnet"
+    resource_group_name = "operator-lab-rg"
+}
+
+module "vm" {
+    source = "github.com/jarpsimoes/tf-modules/virtual-machine"
+
+    name = "database-dev"
+    resource_group_name = "operator-lab-rg"
+    location = "West Europe"
+
+    environment = "shr"
+
+    vm_size = "Standard_b2s"
+
+    network = {
+        internal_address_allocation = "Dynamic"
+        private_ip_address = "10.4.0.100"
+        public_ip = false
+        subnet_id = data.azurerm_subnet.subnet.id
+    }
+
+    image = {
+        publisher = "Canonical"
+        offer     = "0001-com-ubuntu-server-focal"
+        sku       = "20_04-lts-gen2"
+        version   = "20.04.202301130"
+    }
+
+    authentication = {
+        admin_username = "admin_db"
+        pub_key_path = "./key.pub"
+    }
+}
---
+++ terraform-iac/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.35.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "operator-lab-rg"
+    storage_account_name = "operatorlabsa"
+    container_name = "operator-lab-state-container"
+    key = "devdatabase.tfsate"
+  }
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+}
