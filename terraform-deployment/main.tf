5cb59cf57636788c5363ba072fd5c29a1c2b45bb
Joao André Simões <jarpsimoes@gmail.com>
2023-04-18 14:29:22 +0100

    FIX: deployment step

--- terraform-deployment/main.tf
+++ terraform-deployment/main.tf
@@ -21,8 +21,9 @@ resource "azurerm_container_app" "container_app_dev" {
   ingress {
     external_enabled = true
     target_port      = 8080
-    traffic_weight {
-      percentage = 100
+    traffic_weight   {
+      latest_revision = true
+      percentage      = 100
     }
   }
 
@@ -75,78 +76,6 @@ resource "azurerm_container_app" "container_app_dev" {
       env {
           name  = "QUARKUS_HIBERNATE_ORM_DATABASE_GENERATION"
           value = "update"
-      }
-    }
-
-  }
-}
-resource "azurerm_container_app" "container_app_prd" {
-  depends_on = [
-    data.azurerm_container_app_environment.app_env_prd
-  ]
-  name                          = "account-ms-prd"
-  container_app_environment_id  = data.azurerm_container_app_environment.app_env_prd.id
-  resource_group_name           = "operator-lab-rg"
-  revision_mode                 = "Single"
-
-  ingress {
-    external_enabled = true
-    target_port      = 8080
-
-    traffic_weight {
-      percentage = 100
-    }
-  }
-
-  template {
-    min_replicas = 1
-
-    container {
-      name = "ms-account"
-      image = "ghcr.io/jarpsimoes/ms-account:${var.tag}"
-      cpu    = "0.5"
-      memory = "1Gi"
-
-      liveness_probe {
-        port = 8080
-        path = "/health/live"
-        initial_delay = 10
-        transport = "HTTP"
-      }
-      startup_probe {
-        port = 8080
-        path = "/health/ready"
-        transport = "HTTP"
-      }
-      readiness_probe {
-        port = 8080
-        path = "/health/ready"
-        transport = "HTTP"
-      }
-
-      env {
-        name  = "QUARKUS_DATASOURCE_JDBC_URL"
-        value = "jdbc:mysql://10.4.0.100:3306/ms_account_prd"
-      }
-
-      env {
-        name  = "QUARKUS_DATASOURCE_USERNAME"
-        value = "ms_account_prd"
-      }
-
-      env {
-        name  = "QUARKUS_DATASOURCE_PASSWORD"
-        value = "ms_account_prd"
-      }
-
-      env {
-        name  = "QUARKUS_DATASOURCE_DB_KIND"
-        value = "mysql"
-      }
-
-      env {
-        name  = "QUARKUS_HIBERNATE_ORM_DATABASE_GENERATION"
-        value = "update"
       }
     }
 
